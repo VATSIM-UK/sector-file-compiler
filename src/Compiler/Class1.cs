38af9161bda1f1de990ad18c95835047846b8835
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-10-14 23:27:19 +0100

    Setup Project

---
+++ src/Compiler/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Compiler
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/CompilerTest/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CompilerTest
+{
+    public class Class1
+    {
+    }
+}
