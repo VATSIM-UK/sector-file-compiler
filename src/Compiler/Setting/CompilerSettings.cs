e68b46f5f4bb33ad9210d6f85b0a50902922e1c7
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-10-16 21:05:31 +0100

    Add CompilerSettings

---
+++ src/Compiler/Setting/CompilerSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Setting
+{
+    class CompilerSettings
+    {
+        // The place to find the configuration JSON file.
+        private string configFilePath { get; set; }
+    }
+}
