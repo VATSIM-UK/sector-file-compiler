549e2ec937199aa6234b81d278a641b5f47d42c6
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-10-16 21:47:35 +0100

    Add Compiler Entry Point

---
+++ src/Compiler/Compiler.cs
@@ -0,0 +1,14 @@
+﻿using Compiler.Input;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Compiler
+{
+    public class Compiler
+    {
+        public void Compile(List<Argument> arguments, StreamWriter output)
+        {
+            output.Write("Success!");
+        }
+    }
+}
