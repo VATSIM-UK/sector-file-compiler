2f357ea052faf15cffb75ab7e93f5e15010c4572
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-10-23 21:59:37 +0100

    Add Parser For Line Comments

---
+++ tests/CompilerTest/Parser/LineCommentParserTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Compiler.Parser;
+
+namespace CompilerTest.Parser
+{
+    public class LineCommentParserTest
+    {
+        [Fact]
+        public void TestItReturnsEmptyStringIfNoComment()
+        {
+            Assert.Equal("", LineCommentParser.ParseComment("aline withno comment  "));
+        }
+
+        [Fact]
+        public void TestItParsesLineComment()
+        {
+            Assert.Equal("comment", LineCommentParser.ParseComment("aline witha ;comment"));
+        }
+
+        [Fact]
+        public void TestItStripsWhitespaceFromComment()
+        {
+            Assert.Equal("comment", LineCommentParser.ParseComment("aline witha ;   comment  "));
+        }
+    }
+}
