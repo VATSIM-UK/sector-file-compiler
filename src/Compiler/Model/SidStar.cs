c79e7f8409c4e61ce378eaaf689b78bd31423aa3
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-10-23 21:59:58 +0100

    Add Model For Sids and Stars

---
+++ src/Compiler/Model/SidStar.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Compiler.Model
+{
+    public class SidStar : SectorElementModelAbstract
+    {
+        public const string TYPE_SID = "SID";
+
+        public const string TYPE_STAR = "STAR";
+
+        // Whether it is a SID or STAR
+        private readonly string sidstar;
+        public string SidOrStar
+        {
+            get
+            {
+                return this.sidstar;
+            }
+        }
+
+        // The airport the SID or STAR belongs to
+        private readonly string airport;
+        public string Airport
+        {
+            get
+            {
+                return this.airport;
+            }
+        }
+
+        // The runway the SID or STAR belongs to
+        private readonly string runway;
+        public string Runway
+        {
+            get
+            {
+                return this.runway;
+            }
+        }
+
+        // The identifier of the SID or STAR
+        private readonly string identifier;
+        public string Identifier
+        {
+            get
+            {
+                return this.identifier;
+            }
+        }
+
+        // The identifier of the SID or STAR
+        private readonly List<string> route;
+        public List<string> Route
+        {
+            get
+            {
+                return this.route;
+            }
+        }
+
+        public SidStar(
+            string sidstar,
+            string airport,
+            string runway,
+            string identifier,
+            List<string> route,
+            string comment
+        ) : base(comment)
+        {
+            this.sidstar = sidstar;
+            this.airport = airport;
+            this.runway = runway;
+            this.identifier = identifier;
+            this.route = route;
+        }
+
+        public override string BuildOutput()
+        {
+            return base.AddComment(
+                this.sidstar + SectorElementModelAbstract.ITEM_DELIMETER + this.airport + SectorElementModelAbstract.ITEM_DELIMETER +
+                    this.runway + SectorElementModelAbstract.ITEM_DELIMETER + this.identifier + 
+                    SectorElementModelAbstract.ITEM_DELIMETER + String.Join(" ", this.route)
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/SidStarTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Compiler.Model;
+
+namespace CompilerTest.Model
+{
+    public class SidStarTest
+    {
+        private readonly SidStar sidStar;
+
+        public SidStarTest()
+        {
+            this.sidStar = new SidStar(
+                SidStar.TYPE_SID,
+                "EGKK",
+                "26L",
+                "ADMAG2X",
+                new List<string>(new string[] { "FIX1", "FIX2", "FIX3" }),
+                "a lovely comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsType()
+        {
+            Assert.Equal(SidStar.TYPE_SID, this.sidStar.SidOrStar);
+        }
+
+        [Fact]
+        public void TestItSetsAirfield()
+        {
+            Assert.Equal("EGKK", this.sidStar.Airport);
+        }
+
+        [Fact]
+        public void TestItSetsRunway()
+        {
+            Assert.Equal("26L", this.sidStar.Runway);
+        }
+
+        [Fact]
+        public void TestItSetsIdentifier()
+        {
+            Assert.Equal("ADMAG2X", this.sidStar.Identifier);
+        }
+
+        [Fact]
+        public void TestItSetsRoute()
+        {
+            Assert.Equal(new List<string>(new string[] { "FIX1", "FIX2", "FIX3" }), this.sidStar.Route);
+        }
+
+        [Fact]
+        public void TestItFormatsOutput()
+        {
+            string expected = "SID:EGKK:26L:ADMAG2X:FIX1 FIX2 FIX3; a lovely comment";
+            Assert.Equal(expected, this.sidStar.BuildOutput());
+        }
+    }
+}
