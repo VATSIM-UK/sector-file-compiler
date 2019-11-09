33339167b7127a7ca558f3302d80634c9e096a65
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-11-09 19:12:23 +0000

    Add Coordinate

---
+++ src/Compiler/Model/Coordinate.cs
@@ -0,0 +1,23 @@
+﻿namespace Compiler.Model
+{
+    public struct Coordinate
+    {
+        public readonly string latitude;
+        public readonly string longitude;
+
+        public Coordinate(string latitude, string longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "{0} {1}",
+                this.latitude,
+                this.longitude
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/CoordinateTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Compiler.Model;
+using Xunit;
+
+namespace CompilerTest.Model
+{
+    public class CoordinateTest
+    {
+        private readonly Coordinate coordinate;
+
+        public CoordinateTest()
+        {
+            this.coordinate = new Coordinate("abc", "def");
+        }
+
+        [Fact]
+        public void TestItSetsLatitude()
+        {
+            Assert.Equal("abc", coordinate.latitude);
+        }
+
+        [Fact]
+        public void TestItSetsLongitude()
+        {
+            Assert.Equal("def", coordinate.longitude);
+        }
+
+        [Fact]
+        public void TestItRepresentsAsString()
+        {
+            Assert.Equal("abc def", coordinate.ToString());
+        }
+    }
+}
