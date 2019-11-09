ff4d826fb77e2b8cffa25a2b52cdc94d9c01d0ad
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-11-09 21:49:02 +0000

    Add Fix Model

---
+++ tests/CompilerTest/Model/FixTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Compiler.Model;
+using Xunit;
+
+namespace CompilerTest.Model
+{
+    public class FixTest
+    {
+        private readonly Fix fix;
+
+        public FixTest()
+        {
+            this.fix = new Fix("ADMAG", new Coordinate("abc", "def"), "comment");
+        }
+
+        [Fact]
+        public void TestItSetsIdentifier()
+        {
+            Assert.Equal("ADMAG", this.fix.Identifier);
+        }
+
+        [Fact]
+        public void TestItSetsCoordinate()
+        {
+            Assert.Equal(new Coordinate("abc", "def"), this.fix.Coordinate);
+        }
+
+        public void TestItCompiles()
+        {
+            Assert.Equal("ADMAG abc def ;comment", this.fix.Compile());
+        }
+    }
+}
