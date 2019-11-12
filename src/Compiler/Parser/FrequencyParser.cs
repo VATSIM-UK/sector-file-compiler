2261e0760984c667c16fb77409806d8b4117b062
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-11-12 20:48:32 +0000

    Add Frequency Parser

---
+++ src/Compiler/Parser/FrequencyParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Parser
+{
+    public class FrequencyParser : IFrequencyParser
+    {
+        private readonly int firstMinValue;
+        private readonly int firstMaxValue;
+        private readonly int secondDividend;
+
+        public FrequencyParser(int firstMinValue, int firstMaxValue, int secondDividend)
+        {
+            this.firstMinValue = firstMinValue;
+            this.firstMaxValue = firstMaxValue;
+            this.secondDividend = secondDividend;
+        }
+
+        public string ParseFrequency(string frequency)
+        {
+            string[] split = frequency.Split('.');
+            if (split.Length != 2)
+            {
+                return null;
+            }
+
+            if (!int.TryParse(split[0], out int first) || first < firstMinValue || first > firstMaxValue)
+            {
+                return null;
+            }
+
+            if (!int.TryParse(split[1], out int second) || (second % secondDividend) != 0)
+            {
+                return null;
+            }
+
+            return frequency;
+        }
+    }
+}
---
+++ tests/CompilerTest/Parser/FrequencyParserTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Compiler.Parser;
+
+namespace CompilerTest.Parser
+{
+    public class FrequencyParserTest
+    {
+        private readonly FrequencyParser parser;
+
+        public FrequencyParserTest()
+        {
+            this.parser = new FrequencyParser(117, 137, 50);
+        }
+
+        [Theory]
+        [InlineData("a", null)] // Not a frequency
+        [InlineData("123.456.789", null)] // Too many dots
+        [InlineData("123", null)] // Not enough dots
+        [InlineData("abc.500", null)] // First part not an integer
+        [InlineData("116.500", null)] // First part too small
+        [InlineData("138.500", null)] // First part too big
+        [InlineData("135.abc", null)] // Second part not an integer
+        [InlineData("135.155", null)] // Second part doesn't divide
+        [InlineData("135.150", "135.150")] // Everything ok
+        public void TestItParsesFrequencies(string frequency, string expected)
+        {
+            Assert.Equal(expected, this.parser.ParseFrequency(frequency));
+        }
+    }
+}
