a41aee665de80701ae0e6383708859f73067e38f
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-11-16 15:07:34 +0000

    Add ARTCC Model

---
+++ src/Compiler/Model/Artcc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class Artcc : AbstractSectorElement, ICompilable
+    {
+        public Artcc(
+            string identifier,
+            ArtccType type,
+            Coordinate startCoordinate,
+            Coordinate endCoordinate,
+            string comment
+        ) 
+            : base(comment)
+        {
+            Identifier = identifier;
+            Type = type;
+            StartCoordinate = startCoordinate;
+            EndCoordinate = endCoordinate;
+        }
+
+        public string Identifier { get; }
+        public ArtccType Type { get; }
+        public Coordinate StartCoordinate { get; }
+        public Coordinate EndCoordinate { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "{0} {1} {2}{3}",
+                this.Identifier,
+                this.StartCoordinate.ToString(),
+                this.EndCoordinate.ToString(),
+                this.Comment
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/ArtccTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Compiler.Model;
+
+namespace CompilerTest.Model
+{
+    public class ArtccTest
+    {
+        private readonly Artcc artcc;
+
+        public ArtccTest()
+        {
+            this.artcc = new Artcc(
+                "EGTT",
+                ArtccType.HIGH,
+                new Coordinate("abc", "def"),
+                new Coordinate("ghi", "jkl"),
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsIdentifier()
+        {
+            Assert.Equal("EGTT", this.artcc.Identifier);
+        }
+
+        [Fact]
+        public void TestItSetsType()
+        {
+            Assert.Equal(ArtccType.HIGH, this.artcc.Type);
+        }
+
+        [Fact]
+        public void TestItSetsStartCoordinate()
+        {
+            Assert.Equal(new Coordinate("abc", "def"), this.artcc.StartCoordinate);
+        }
+
+        [Fact]
+        public void TestItSetsEndCoordinate()
+        {
+            Assert.Equal(new Coordinate("ghi", "jkl"), this.artcc.EndCoordinate);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal("EGTT abc def ghi jkl ;comment", this.artcc.Compile());
+        }
+    }
+}
