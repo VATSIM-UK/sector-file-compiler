de1421657dd4ea3be1fdf733a75fe634617084e4
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-11-16 14:41:37 +0000

    Add SID/STAR Route Validation

---
+++ src/Compiler/Validate/AllSidsMustHaveAValidRoute.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Compiler.Event;
+using Compiler.Model;
+using Compiler.Error;
+
+namespace Compiler.Validate
+{
+    public class AllSidsMustHaveAValidRoute : IValidationRule
+    {
+        public void Validate(SectorElementCollection sectorElements, IEventLogger events)
+        {
+            foreach (SidStar sidStar in sectorElements.SidStars)
+            {
+                foreach (string waypoint in sidStar.Route)
+                {
+                    if (
+                        !FindFixByIdentifier(waypoint, sectorElements) &&
+                        !FindVorByIdentifier(waypoint, sectorElements) &&
+                        !FindNdbByIdentifier(waypoint, sectorElements) &&
+                        !FindAirportByIdentifier(waypoint, sectorElements)
+                    ) {
+                        string message = String.Format(
+                            "Invalid waypoint {0} on SID/STAR {1}/{2}",
+                            waypoint,
+                            sidStar.Airport,
+                            sidStar.Identifier
+                        );
+                        events.AddEvent(
+                            new ValidationRuleFailure(message)
+                        );
+                    }
+                }
+            }
+        }
+
+        private bool FindVorByIdentifier(string identifier, SectorElementCollection sectorElements)
+        {
+            foreach (Vor vor in sectorElements.Vors)
+            {
+                if (vor.Identifier == identifier)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool FindNdbByIdentifier(string identifier, SectorElementCollection sectorElements)
+        {
+            foreach (Ndb ndb in sectorElements.Ndbs)
+            {
+                if (ndb.Identifier == identifier)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool FindAirportByIdentifier(string identifier, SectorElementCollection sectorElements)
+        {
+            foreach (Airport airport in sectorElements.Airports)
+            {
+                if (airport.Icao == identifier)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool FindFixByIdentifier(string identifier, SectorElementCollection sectorElements)
+        {
+            foreach (Fix fix in sectorElements.Fixes)
+            {
+                if (fix.Identifier == identifier)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tests/CompilerTest/Event/CompilationMessageTest.cs
@@ -0,0 +1,27 @@
+﻿using Compiler.Event;
+using Xunit;
+
+namespace CompilerTest.Event
+{
+    public class CompilationMessageTest
+    {
+        private readonly CompilationMessage message;
+
+        public CompilationMessageTest()
+        {
+            this.message = new CompilationMessage("This is a test message");
+        }
+
+        [Fact]
+        public void TestItIsNonFatal()
+        {
+            Assert.False(this.message.IsFatal());
+        }
+
+        [Fact]
+        public void TestOutput()
+        {
+            Assert.Equal("INFO: This is a test message", this.message.GetMessage());
+        }
+    }
+}
