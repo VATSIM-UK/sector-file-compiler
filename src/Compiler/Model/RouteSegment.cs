51b4dff50bc32afaff3e1db24f1e54a06212b054
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-01-05 19:07:03 +0000

    Add Compilable For Route Segments
    This features a lot in SIDs and STARs

---
+++ src/Compiler/Model/RouteSegment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class RouteSegment : AbstractSectorElement, ICompilable
+    {
+        public RouteSegment(
+            Point start,
+            Point end,
+            string comment
+        )
+            : base(comment)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public Point Start { get; }
+        public Point End { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "{0} {1}{2}\r\n",
+                this.Start.Compile(),
+                this.End.Compile(),
+                this.CompileComment()
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/RouteSegmentTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Compiler.Model;
+
+namespace CompilerTest.Model
+{
+    public class RouteSegmentTest
+    {
+        private readonly RouteSegment segment;
+
+        private List<Point> points;
+
+        public RouteSegmentTest()
+        {
+            this.segment = new RouteSegment(
+                new Point("BIG"),
+                new Point("LAM"),
+                null
+            );
+        }
+
+        [Fact]
+        public void TestItSetsStart()
+        {
+            Assert.Equal(new Point("BIG"), this.segment.Start);
+        }
+
+        [Fact]
+        public void TestItSetsEnd()
+        {
+            Assert.Equal(new Point("LAM"), this.segment.End);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "BIG BIG LAM LAM\r\n",
+                this.segment.Compile()
+            );
+        }
+
+        [Fact]
+        public void TestItCompilesWithComment()
+        {
+            RouteSegment segment = new RouteSegment(
+                new Point("BIG"),
+                new Point("LAM"),
+                "Foo"
+            );
+
+            Assert.Equal(
+                "BIG BIG LAM LAM ;Foo\r\n",
+                segment.Compile()
+            );
+        }
+    }
+}
