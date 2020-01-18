88c05beb42341758ce5fe4f80c3188c40bb73bc3
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-01-18 13:52:15 +0000

    Add Geo To Collection

--- tests/CompilerTest/Model/SectorElementCollectionTest.cs
+++ tests/CompilerTest/Model/SectorElementCollectionTest.cs
@@ -237,6 +237,24 @@ namespace CompilerTest.Model
         }
 
         [Fact]
+        public void TestItAddsGeo()
+        {
+            Geo geo = new Geo(new Coordinate("abc", "def"), new Coordinate("ghi", "jkl"), "red", null);
+            this.collection.Add(geo);
+
+            Assert.Equal(geo, this.collection.GeoElements[0]);
+        }
+
+        [Fact]
+        public void TestItAddsGeoToCompilableSection()
+        {
+            Geo geo = new Geo(new Coordinate("abc", "def"), new Coordinate("ghi", "jkl"), "red", null);
+            this.collection.Add(geo);
+
+            Assert.Equal(geo, this.collection.Compilables[OutputSections.SCT_GEO][0]);
+        }
+
+        [Fact]
         public void TestItAddsVors()
         {
             Vor vor = new Vor("a", "123.456", new Coordinate("abc", "def"), "test");
