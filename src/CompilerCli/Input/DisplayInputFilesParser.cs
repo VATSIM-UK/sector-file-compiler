3689f48fe1c2b062bd522a129c67dd6f75578e01
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-01-18 22:30:48 +0000

    Add Class To Parse Command Lind Argument

---
+++ src/CompilerCli/Input/DisplayInputFilesParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Compiler.Argument;
+
+namespace CompilerCli.Input
+{
+    public class DisplayInputFilesParser : IInputParser
+    {
+        public CompilerArguments Parse(List<string> values, CompilerArguments compilerSettings)
+        {
+            compilerSettings.DisplayInputFiles = true;
+            return compilerSettings;
+        }
+    }
+}
