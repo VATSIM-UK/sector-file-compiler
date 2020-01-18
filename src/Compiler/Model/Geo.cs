5d991c503d68a69d61051273575092647185040a
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-01-18 13:48:56 +0000

    Add Model For Geo

---
+++ src/Compiler/Model/Geo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Compiler.Model
+{
+    public class Geo : AbstractSectorElement, ICompilable
+    {
+        public Geo(
+            Coordinate startPoint,
+            Coordinate endPoint,
+            string colour,
+            string comment
+        ) : base(comment)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+            Colour = colour;
+        }
+
+        public Coordinate StartPoint { get; }
+        public Coordinate EndPoint { get; }
+        public string Colour { get; }
+
+        public string Compile()
+        {
+            return string.Format(
+                "{0} {1} {2}{3}\r\n",
+                this.StartPoint.ToString(),
+                this.EndPoint.ToString(),
+                this.Colour,
+                this.CompileComment()
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/GeoTest.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using Compiler.Model;
+
+namespace CompilerTest.Model
+{
+    public class GeoTest
+    {
+        private readonly Geo model;
+
+        public GeoTest()
+        {
+            this.model = new Geo(
+                new Coordinate("abc", "def"),
+                new Coordinate("ghi", "jkl"),
+                "Blue",
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsStartPoint()
+        {
+            Assert.Equal(new Coordinate("abc", "def"), this.model.StartPoint);
+        }
+
+        [Fact]
+        public void TestItSetsEndPoint()
+        {
+            Assert.Equal(new Coordinate("ghi", "jkl"), this.model.EndPoint);
+        }
+
+        [Fact]
+        public void TestItSetsColour()
+        {
+            Assert.Equal("Blue", this.model.Colour);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal("abc def ghi jkl Blue ;comment\r\n", this.model.Compile());
+        }
+    }
+}
