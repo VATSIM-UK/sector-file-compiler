f0b0b1089e2ec88cdf9145078ff3cb6be15f5c10
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-01-19 17:43:58 +0000

    Add Validator For Colours

---
+++ src/Compiler/Validate/ColourValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Compiler.Model;
+
+namespace Compiler.Validate
+{
+    public class ColourValidator
+    {
+        public static bool ColourValid(SectorElementCollection sectorElements, string colour)
+        {
+            return IsValidColourInteger(colour) || ColourIsDefined(sectorElements, colour);
+        }
+
+        public static bool IsValidColourInteger(string colour)
+        {
+            return (int.TryParse(colour, out int colourValue) && colourValue >= 0 && colourValue <= 16777215);
+        }
+
+        public static bool ColourIsDefined(SectorElementCollection sectorElements, string colourString)
+        {
+            foreach (Colour colour in sectorElements.Colours)
+            {
+                if (colourString == colour.Name)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tests/CompilerTest/Validate/ColourValidatorTest.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+using Compiler.Model;
+using Compiler.Event;
+using Compiler.Error;
+using Compiler.Validate;
+using Moq;
+using Compiler.Argument;
+
+namespace CompilerTest.Validate
+{
+    public class ColourValidatorTest
+    {
+        private readonly SectorElementCollection sectorElements;
+        private readonly Mock<IEventLogger> loggerMock;
+        private readonly Colour first;
+        private readonly Colour second;
+
+        public ColourValidatorTest()
+        {
+            this.sectorElements = new SectorElementCollection();
+            this.loggerMock = new Mock<IEventLogger>();
+            this.first = new Colour("colour1", 1, "test");
+            this.second = new Colour("colour2", 555, "test");
+            this.sectorElements.Add(this.first);
+            this.sectorElements.Add(this.second);
+        }
+
+        [Theory]
+        [InlineData ("0")]
+        [InlineData ("1")]
+        [InlineData ("255")]
+        [InlineData ("16777214")]
+        [InlineData ("16777215")]
+        public void TestItPassesOnValidColourIntegers(string value)
+        {
+            Assert.True(ColourValidator.IsValidColourInteger(value));
+        }
+
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("abc")]
+        [InlineData("red")]
+        [InlineData("123.4")]
+        [InlineData("16777215.0")]
+        [InlineData("16777216")]
+        public void TestItFailsOnInvalidColourIntegers(string value)
+        {
+            Assert.False(ColourValidator.IsValidColourInteger(value));
+        }
+
+        [Theory]
+        [InlineData("colour1")]
+        [InlineData("colour2")]
+        public void TestItPassesIfColourIsDefined(string colour)
+        {
+            Assert.True(ColourValidator.ColourIsDefined(this.sectorElements, colour));
+        }
+
+        [Theory]
+        [InlineData("123")]
+        [InlineData("colour1")]
+        [InlineData("colour2")]
+        public void TestItValidatesColour(string colour)
+        {
+            Assert.True(ColourValidator.ColourValid(this.sectorElements, colour));
+        }
+
+        [Theory]
+        [InlineData("123.4")]
+        [InlineData("colour3")]
+        [InlineData("-1")]
+        public void TestItFailsToValidateColour(string colour)
+        {
+            Assert.False(ColourValidator.ColourValid(this.sectorElements, colour));
+        }
+    }
+}
