55f6c99f3b106321cc78111c22dbfcc1a25a74c5
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-01-19 13:00:22 +0000

    Add Label Model

---
+++ src/Compiler/Model/Label.cs
@@ -0,0 +1,27 @@
+﻿namespace Compiler.Model
+{
+    public class Label : AbstractSectorElement, ICompilable
+    {
+        public Label(string text, Coordinate position, string colour, string comment) : base(comment)
+        {
+            Text = text;
+            Position = position;
+            Colour = colour;
+        }
+
+        public string Text { get; }
+        public Coordinate Position { get; }
+        public string Colour { get; }
+
+        public string Compile()
+        {
+            return string.Format(
+                "\"{0}\" {1} {2}{3}\r\n",
+                this.Text,
+                this.Position.ToString(),
+                this.Colour,
+                this.CompileComment()
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/LabelTest.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using Compiler.Model;
+
+namespace CompilerTest.Model
+{
+    public class LabelTest
+    {
+        private readonly Label label;
+
+        public LabelTest()
+        {
+            this.label = new Label("label 1", new Coordinate("abc", "def"), "colour", null);
+        }
+
+        [Fact]
+        public void TestItSetsText()
+        {
+            Assert.Equal("label 1", this.label.Text);
+        }
+
+        [Fact]
+        public void TestItSetsPosition()
+        {
+            Assert.Equal(new Coordinate("abc", "def"), this.label.Position);
+        }
+
+        [Fact]
+        public void TestItSetsColour()
+        {
+            Assert.Equal("colour", this.label.Colour);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal("\"label 1\" abc def colour\r\n", this.label.Compile());
+        }
+
+        [Fact]
+        public void TestItCompilesWithComment()
+        {
+            Assert.Equal(
+                "\"label 1\" abc def colour ;comment\r\n",
+                new Label("label 1", new Coordinate("abc", "def"), "colour", "comment").Compile()
+            );
+        }
+    }
+}
