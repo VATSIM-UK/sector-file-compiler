6a49bba52def09022c191acd2c14c5b83af11ffd
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-01-19 16:13:22 +0000

    Add Label Format Parser

---
+++ src/Compiler/Parser/SctLabelLineParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Parser
+{
+    public class SctLabelLineParser : ISectorLineParser
+    {
+        public SectorFormatLine ParseLine(string line)
+        {
+            string comment = LineCommentParser.ParseComment(line);
+            string data = LineCommentParser.ParseData(line).TrimStart();
+
+            // Find the quotes
+            if (data[0] != '"')
+            {
+                return new SectorFormatLine(
+                    "",
+                    new List<string>(),
+                    null
+                );
+            }
+
+            int secondQuote = data.IndexOf('"', 1);
+            if (secondQuote == -1)
+            {
+                return new SectorFormatLine(
+                    "",
+                    new List<string>(),
+                    null
+                );
+            }
+
+            string label = data.Substring(1, secondQuote - 1);
+            List<string> dataSplit = new List<string>(data.Substring(secondQuote + 1).Split(null));
+            dataSplit.RemoveAll(s => s == "");
+            dataSplit.Insert(0, label);
+
+            return new SectorFormatLine(
+                data,
+                dataSplit,
+                comment
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Parser/SctLabelLineParserTest.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Compiler.Parser;
+
+namespace CompilerTest.Parser
+{
+    public class SctLabelLineParserTest
+    {
+        private readonly SctLabelLineParser parser;
+
+        public SctLabelLineParserTest()
+        {
+            this.parser = new SctLabelLineParser();
+        }
+
+        [Fact]
+        public void TestItParsesLinesWithNoComments()
+        {
+            SectorFormatLine expected = new SectorFormatLine(
+                "\"test label\" abc def ghi",
+                new List<string>(new string[] { "test label", "abc", "def", "ghi" }),
+                null
+            );
+            Assert.True(expected.Equals(this.parser.ParseLine("\"test label\" abc def ghi")));
+        }
+
+        [Fact]
+        public void TestItParsesLinesWithComments()
+        {
+            SectorFormatLine expected = new SectorFormatLine(
+                "\"test label\" abc def ghi",
+                new List<string>(new string[] { "test label", "abc", "def", "ghi" }),
+                "comment"
+            );
+
+            Assert.True(expected.Equals(this.parser.ParseLine("\"test label\" abc def ghi ;comment")));
+        }
+
+        [Fact]
+        public void TestItHandlesDoubleSpaces()
+        {
+            SectorFormatLine expected = new SectorFormatLine(
+                "\"test label\" abc def    ghi",
+                new List<string>(new string[] { "test label", "abc", "def", "ghi" }),
+                "comment"
+            );
+
+            Assert.True(expected.Equals(this.parser.ParseLine("\"test label\" abc def    ghi ;comment")));
+        }
+
+        [Fact]
+        public void TestItReturnsBlankOnNoStartingDoubleQuote()
+        {
+            SectorFormatLine expected = new SectorFormatLine(
+                    "",
+                    new List<string>(),
+                    null
+            );
+            Assert.True(
+                expected.Equals(this.parser.ParseLine("test label abc def    ghi ;comment"))
+            );
+        }
+
+        [Fact]
+        public void TestItReturnsBlankOnCharacterBeforeFirstDoubleQuote()
+        {
+            SectorFormatLine expected = new SectorFormatLine(
+                    "",
+                    new List<string>(),
+                    null
+            );
+            Assert.True(
+                expected.Equals(this.parser.ParseLine("a\"test label abc def    ghi ;comment"))
+            );
+        }
+
+        [Fact]
+        public void TestItReturnsBlankOnNoEndingDoubleQuote()
+        {
+            SectorFormatLine expected = new SectorFormatLine(
+                    "",
+                    new List<string>(),
+                    null
+            );
+            Assert.True(
+                expected.Equals(this.parser.ParseLine("\"test label abc def    ghi ;comment"))
+            );
+        }
+    }
+}
