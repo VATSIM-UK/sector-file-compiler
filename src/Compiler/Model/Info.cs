5051490332a87ec1c8ec465d57a824411aafb13b
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-04-08 21:29:02 +0100

    Add Model For Info

---
+++ src/Compiler/Model/Info.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class Info : AbstractSectorElement, ICompilable
+    {
+        public Info(
+            string name,
+            string callsign,
+            string airport,
+            Coordinate coordinate,
+            int milesPerDegreeLatitude,
+            int milesPerDegreeLongitude,
+            int magneticVariation,
+            int scale
+        ) : base("")
+        {
+            Name = name;
+            Callsign = callsign;
+            Airport = airport;
+            Coordinate = coordinate;
+            MilesPerDegreeLatitude = milesPerDegreeLatitude;
+            MilesPerDegreeLongitude = milesPerDegreeLongitude;
+            MagneticVariation = magneticVariation;
+            Scale = scale;
+        }
+
+        public string Name { get; }
+        public string Callsign { get; }
+        public string Airport { get; }
+        public Coordinate Coordinate { get; }
+        public int MilesPerDegreeLatitude { get; }
+        public int MilesPerDegreeLongitude { get; }
+        public int MagneticVariation { get; }
+        public int Scale { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "{0}\r\n{1}\r\n{2}\r\n{3}\r\n{4}\r\n{5}\r\n{6}\r\n{7}\r\n{8}\r\n",
+                this.Name,
+                this.Callsign,
+                this.Airport,
+                this.Coordinate.latitude,
+                this.Coordinate.longitude,
+                this.MilesPerDegreeLatitude,
+                this.MilesPerDegreeLongitude,
+                this.MagneticVariation,
+                this.Scale
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/InfoTest.cs
@@ -0,0 +1,81 @@
+﻿using Xunit;
+using Compiler.Model;
+
+namespace CompilerTest.Model
+{
+    public class InfoTest
+    {
+        private readonly Info model;
+
+        public InfoTest()
+        {
+            this.model = new Info(
+                "Super Cool Sector",
+                "LON_CTR",
+                "EGLL",
+                new Coordinate("123", "456"),
+                60,
+                40,
+                2,
+                1
+            );
+        }
+
+        [Fact]
+        public void TestItSetsName()
+        {
+            Assert.Equal("Super Cool Sector", this.model.Name);
+        }
+
+        [Fact]
+        public void TestItSetsCallsign()
+        {
+            Assert.Equal("LON_CTR", this.model.Callsign);
+        }
+
+        [Fact]
+        public void TestItSetsAirport()
+        {
+            Assert.Equal("EGLL", this.model.Airport);
+        }
+
+        [Fact]
+        public void TestItSetsCoordinate()
+        {
+            Assert.Equal(new Coordinate("123", "456"), this.model.Coordinate);
+        }
+
+        [Fact]
+        public void TestItSetsMilesPerDegreeLatitude()
+        {
+            Assert.Equal(60, this.model.MilesPerDegreeLatitude);
+        }
+
+        [Fact]
+        public void TestItSetsMilesPerDegreeLongitude()
+        {
+            Assert.Equal(40, this.model.MilesPerDegreeLongitude);
+        }
+
+        [Fact]
+        public void TestItSetsMagneticVariation()
+        {
+            Assert.Equal(2, this.model.MagneticVariation);
+        }
+
+        [Fact]
+        public void TestItSetsScale()
+        {
+            Assert.Equal(1, this.model.Scale);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "Super Cool Sector\r\nLON_CTR\r\nEGLL\r\n123\r\n456\r\n60\r\n40\r\n2\r\n1\r\n",
+                this.model.Compile()
+            );
+        }
+    }
+}
