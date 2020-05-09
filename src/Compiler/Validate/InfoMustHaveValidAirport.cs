3d5f78e8e67910f0f9a83af59a64d22a92e4aa9b
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-05-09 19:12:17 +0100

    Validate That Info Has Valid Airport

---
+++ src/Compiler/Validate/InfoMustHaveValidAirport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Compiler.Event;
+using Compiler.Model;
+using Compiler.Error;
+using Compiler.Argument;
+
+namespace Compiler.Validate
+{
+    public class InfoMustHaveValidAirport : IValidationRule
+    {
+        public void Validate(SectorElementCollection sectorElements, CompilerArguments args, IEventLogger events)
+        {
+            foreach(Airport airport in sectorElements.Airports)
+            {
+                if (airport.Icao == sectorElements.Info.Airport)
+                {
+                    return;
+                }
+            }
+
+            events.AddEvent(
+                new ValidationRuleFailure("Invalid airport in INFO definition")
+            );
+        }       
+    }
+}
---
+++ tests/CompilerTest/Validate/InfoMustHaveValidAirportTest.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using Moq;
+using Compiler.Model;
+using Compiler.Validate;
+using Compiler.Event;
+using Compiler.Error;
+using Compiler.Argument;
+
+namespace CompilerTest.Validate
+{
+    public class InfoMustHaveValidAirportTest
+    {
+        private readonly Airport airfield1;
+        private readonly Airport airfield2;
+        private readonly Airport airfield3;
+        private readonly SectorElementCollection elements;
+        private readonly InfoMustHaveValidAirport validator;
+        private readonly Mock<IEventLogger> loggerMock;
+        private readonly CompilerArguments args;
+
+        public InfoMustHaveValidAirportTest()
+        {
+            this.elements = new SectorElementCollection();
+            this.elements.Add(new Airport("a", "EGKK", new Coordinate("a", "b"), "a", "c"));
+            this.elements.Add(new Airport("a", "EGLL", new Coordinate("a", "b"), "a", "c"));
+            this.loggerMock = new Mock<IEventLogger>();
+            this.validator = new InfoMustHaveValidAirport();
+            this.args = new CompilerArguments();
+        }
+
+        [Fact]
+        public void TestItPassesOnValidAirport()
+        {
+            this.elements.Add(new Info(
+                "Test",
+                "LON_CTR",
+                "EGLL",
+                new Coordinate("abc", "def"),
+                0,
+                0,
+                0,
+                0
+            ));
+
+            this.validator.Validate(this.elements, this.args, this.loggerMock.Object);
+            this.loggerMock.Verify(foo => foo.AddEvent(It.IsAny<ValidationRuleFailure>()), Times.Never);
+        }
+
+        [Fact]
+        public void TestItFailsOnInvalidAirport()
+        {
+            this.elements.Add(new Info(
+                "Test",
+                "LON_CTR",
+                "EGXX",
+                new Coordinate("abc", "def"),
+                0,
+                0,
+                0,
+                0
+            ));
+
+            this.validator.Validate(this.elements, this.args, this.loggerMock.Object);
+            this.loggerMock.Verify(foo => foo.AddEvent(It.IsAny<ValidationRuleFailure>()), Times.Once);
+        }
+    }
+}
