eb7aee705ab68f36b2209e68d1d7b39cf512e0ee
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-05-13 14:48:55 +0100

    Add Event For Parse Time Suggestions

---
+++ tests/CompilerTest/Event/ParserSuggestionTest.cs
@@ -0,0 +1,27 @@
+﻿using Compiler.Event;
+using Xunit;
+
+namespace CompilerTest.Event
+{
+    public class ParserSuggestionTest
+    {
+        private readonly ParserSuggestion message;
+
+        public ParserSuggestionTest()
+        {
+            this.message = new ParserSuggestion("This is a test suggestion");
+        }
+
+        [Fact]
+        public void TestItIsNonFatal()
+        {
+            Assert.False(this.message.IsFatal());
+        }
+
+        [Fact]
+        public void TestOutput()
+        {
+            Assert.Equal("SUGGESTION: This is a test suggestion", this.message.GetMessage());
+        }
+    }
+}
