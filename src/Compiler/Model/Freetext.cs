a2c30a4eed447611485a01c9898acfa7508eb556
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-05-13 14:01:43 +0100

    Add Freetext Model

---
+++ src/Compiler/Model/Freetext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class Freetext : AbstractSectorElement, ICompilable
+    {
+        public Freetext(string title, string text, Coordinate coordinate, string comment)
+            : base(comment)
+        {
+            Title = title;
+            Text = text;
+            Coordinate = coordinate;
+        }
+
+        public string Title { get; }
+        public string Text { get; }
+        public Coordinate Coordinate { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "{0}:{1}:{2}:{3}{4}\r\n",
+                this.Coordinate.latitude,
+                this.Coordinate.longitude,
+                this.Title,
+                this.Text,
+                this.CompileComment()
+            );
+        }
+    }
+}
