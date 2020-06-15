3689ddf0b49b9fd69344721699eed3fdf1028eed
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-15 21:25:37 +0100

    Add Alternate Hierarchy

---
+++ src/Compiler/Model/SectorAlternateOwnerHierarchy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class SectorAlternateOwnerHierarchy : AbstractSectorElement, ICompilable
+    {
+        public SectorAlternateOwnerHierarchy(
+            string name,
+            List<string> owners,
+            string comment
+        ) : base(comment) 
+        {
+            Name = name;
+            Owners = owners;
+        }
+
+        public string Name { get; }
+        public List<string> Owners { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "ALTOWNER:{0}:{1}{2}\r\n",
+                this.Name,
+                string.Join(':', this.Owners),
+                this.CompileComment()
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/SectorAlternateOwnerHierarchyTest.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using Compiler.Model;
+using System.Collections.Generic;
+
+namespace CompilerTest.Model
+{
+    public class SectorAlternateOwnerHierarchyTest
+    {
+        private readonly SectorAlternateOwnerHierarchy model;
+        private List<string> owners = new List<string>
+        {
+            "ONE",
+            "TWO",
+            "THREE"
+        };
+
+        public SectorAlternateOwnerHierarchyTest()
+        {
+            this.model = new SectorAlternateOwnerHierarchy(
+                "TEST",
+                this.owners,
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsName()
+        {
+            Assert.Equal("TEST", this.model.Name);
+        }
+
+        [Fact]
+        public void TestItSetsOwners()
+        {
+            Assert.Equal(this.owners, this.model.Owners);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "ALTOWNER:TEST:ONE:TWO:THREE ;comment\r\n",
+                this.model.Compile()
+            );
+        }
+    }
+}
