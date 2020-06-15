4e8812ea0d087c9d41b3fd3cf137e25065eaa58b
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-15 21:35:38 +0100

    Model For SectorActive

---
+++ src/Compiler/Model/SectorActive.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class SectorActive : AbstractSectorElement, ICompilable
+    {
+        public SectorActive(
+            string airfield,
+            string runway,
+            string comment
+        ) : base(comment) 
+        {
+            Airfield = airfield;
+            Runway = runway;
+        }
+
+        public string Airfield { get; }
+        public string Runway { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "ACTIVE:{0}:{1}{2}\r\n",
+                this.Airfield,
+                this.Runway,
+                this.CompileComment()
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/SectorActiveTest.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using Compiler.Model;
+using System.Collections.Generic;
+
+namespace CompilerTest.Model
+{
+    public class SectorActiveTest
+    {
+        private readonly SectorActive model;
+
+        public SectorActiveTest()
+        {
+            this.model = new SectorActive(
+                "EGLL",
+                "09R",
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsAirfield()
+        {
+            Assert.Equal("EGLL", this.model.Airfield);
+        }
+
+        [Fact]
+        public void TestItSetsRunway()
+        {
+            Assert.Equal("09R", this.model.Runway);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "ACTIVE:EGLL:09R ;comment\r\n",
+                this.model.Compile()
+            );
+        }
+    }
+}
