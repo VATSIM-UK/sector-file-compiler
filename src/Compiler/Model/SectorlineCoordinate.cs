28284fe7209837b4ee56a663d45329dcb613ede5
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-15 21:00:38 +0100

    Model For Coordinates

---
+++ src/Compiler/Model/SectorlineCoordinate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class SectorlineCoordinate : AbstractSectorElement, ICompilable
+    {
+        public SectorlineCoordinate(
+            Coordinate corodinate,
+            string comment
+        ) : base(comment) 
+        {
+            Corodinate = corodinate;
+        }
+
+        public Coordinate Corodinate { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "COORD:{0}:{1}{2}\r\n",
+                this.Corodinate.latitude,
+                this.Corodinate.longitude,
+                this.CompileComment()
+            );
+        }
+    }
+}
