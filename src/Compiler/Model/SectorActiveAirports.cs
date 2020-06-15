3f751ba424dbfb38ebc2f5de103edba3fe76bd01
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-15 21:47:27 +0100

    Model For SectorActiveAirports

---
+++ src/Compiler/Model/SectorActiveAirports.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class SectorActiveAirports : AbstractSectorElement, ICompilable
+    {
+        public SectorActiveAirports(
+            bool departureAirport,
+            List<string> airports,
+            string comment
+        ) : base(comment) 
+        {
+            DepartureAirport = departureAirport;
+            Airports = airports;
+        }
+
+        public bool DepartureAirport { get; }
+        public List<string> Airports { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "{0}:{1}{2}\r\n",
+                this.DepartureAirport ? "DEPAPT" : "ARRAPT",
+                string.Join(":", this.Airports),
+                this.CompileComment()
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/SectorActiveAirportsTest.cs
@@ -0,0 +1,61 @@
+﻿using Xunit;
+using Compiler.Model;
+using System.Collections.Generic;
+
+namespace CompilerTest.Model
+{
+    public class SectorActiveAirportsTest
+    {
+        private readonly SectorActiveAirports model;
+        private readonly List<string> airports = new List<string>
+        {
+            "EGKK",
+            "EGLL"
+        };
+
+        public SectorActiveAirportsTest()
+        {
+            this.model = new SectorActiveAirports(
+                true,
+                this.airports,
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsDepartureAirport()
+        {
+            Assert.True(this.model.DepartureAirport);
+        }
+
+        [Fact]
+        public void TestItSetsAirports()
+        {
+            Assert.Equal(this.airports, this.model.Airports);
+        }
+
+        [Fact]
+        public void TestItCompilesDepartureAirports()
+        {
+            Assert.Equal(
+                "DEPAPT:EGKK:EGLL ;comment\r\n",
+                this.model.Compile()
+            );
+        }
+
+        [Fact]
+        public void TestItCompilesArrivalAirports()
+        {
+            SectorActiveAirports model2 = new SectorActiveAirports(
+                false,
+                this.airports,
+                "comment"
+            );
+
+            Assert.Equal(
+                "ARRAPT:EGKK:EGLL ;comment\r\n",
+                model2.Compile()
+            );
+        }
+    }
+}
