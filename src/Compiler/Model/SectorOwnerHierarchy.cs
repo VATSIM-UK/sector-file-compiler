6aada688bcbaec0715cc72244dedf26614938cbc
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-15 21:23:03 +0100

    Add Model For Sector Owner Hierarchy

---
+++ src/Compiler/Model/SectorOwnerHierarchy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class SectorOwnerHierarchy : AbstractSectorElement, ICompilable
+    {
+        public SectorOwnerHierarchy(
+            List<string> owners,
+            string comment
+        ) : base(comment) 
+        {
+            Owners = owners;
+        }
+
+        public List<string> Owners { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "OWNER:{0}{1}\r\n",
+                string.Join(':', this.Owners),
+                this.CompileComment()
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/SectorOwnerHierarchyTest.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using Compiler.Model;
+using System.Collections.Generic;
+
+namespace CompilerTest.Model
+{
+    public class SectorOwnerHierarchyTest
+    {
+        private readonly SectorOwnerHierarchy model;
+        private List<string> owners = new List<string>
+        {
+            "ONE",
+            "TWO",
+            "THREE"
+        };
+
+        public SectorOwnerHierarchyTest()
+        {
+            this.model = new SectorOwnerHierarchy(
+                this.owners,
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsOwners()
+        {
+            Assert.Equal(this.owners, this.model.Owners);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "OWNER:ONE:TWO:THREE ;comment\r\n",
+                this.model.Compile()
+            );
+        }
+    }
+}
