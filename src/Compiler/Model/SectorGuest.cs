32a965b29e7433996e1c891ae0f3f6c654e227bc
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-15 21:41:24 +0100

    Add SectorGuest Model

---
+++ src/Compiler/Model/SectorGuest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class SectorGuest : AbstractSectorElement, ICompilable
+    {
+        public SectorGuest(
+            string sector,
+            string departureAirport,
+            string arrivalAirport,
+            string comment
+        ) : base(comment) 
+        {
+            Sector = sector;
+            DepartureAirport = departureAirport;
+            ArrivalAirport = arrivalAirport;
+        }
+
+        public string Sector { get; }
+        public string DepartureAirport { get; }
+        public string ArrivalAirport { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "GUEST:{0}:{1}:{2}{3}\r\n",
+                this.Sector,
+                this.DepartureAirport,
+                this.ArrivalAirport,
+                this.CompileComment()
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/SectorGuestTest.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using Compiler.Model;
+using System.Collections.Generic;
+
+namespace CompilerTest.Model
+{
+    public class SectorGuestTest
+    {
+        private readonly SectorGuest model;
+
+        public SectorGuestTest()
+        {
+            this.model = new SectorGuest(
+                "MWAL",
+                "EGBB",
+                "*",
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsSector()
+        {
+            Assert.Equal("MWAL", this.model.Sector);
+        }
+
+        [Fact]
+        public void TestItSetsDepartureAirport()
+        {
+            Assert.Equal("EGBB", this.model.DepartureAirport);
+        }
+
+        [Fact]
+        public void TestItSetsArrivalAirport()
+        {
+            Assert.Equal("*", this.model.ArrivalAirport);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "GUEST:MWAL:EGBB:* ;comment\r\n",
+                this.model.Compile()
+            );
+        }
+    }
+}
