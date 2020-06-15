4b913eb39950859ce7135a0442527c1841675a62
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-15 21:29:01 +0100

    Add Model For Sector Borders

---
+++ src/Compiler/Model/SectorBorder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class SectorBorder : AbstractSectorElement, ICompilable
+    {
+        public SectorBorder(
+            List<string> borderLines,
+            string comment
+        ) : base(comment) 
+        {
+            BorderLines = borderLines;
+        }
+
+        public List<string> BorderLines { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "BORDER:{0}{1}\r\n",
+                string.Join(':', this.BorderLines),
+                this.CompileComment()
+            );
+        }
+    }
+}
