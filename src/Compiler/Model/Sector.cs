d074033a216185971d27d978ae10b7130fcf6c70
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-17 21:00:51 +0100

    Overall Sector Model Tweaks

--- src/Compiler/Model/Sector.cs
+++ src/Compiler/Model/Sector.cs
@@ -13,10 +13,11 @@ namespace Compiler.Model
             int maximumAltitude,
             SectorOwnerHierarchy owners,
             List<SectorAlternateOwnerHierarchy> altOwners,
+            List<SectorActive> active,
             List<SectorGuest> guests,
             SectorBorder border,
-            List<SectorActiveAirports> arrivalAirports,
-            List<SectorActiveAirports> departureAirport,
+            SectorArrivalAirports arrivalAirports,
+            SectorDepartureAirports departureAirports,
             string definitionComment
         ) : base(definitionComment) 
         {
@@ -25,10 +26,11 @@ namespace Compiler.Model
             MaximumAltitude = maximumAltitude;
             Owners = owners;
             AltOwners = altOwners;
+            Active = active;
             Guests = guests;
             Border = border;
             ArrivalAirports = arrivalAirports;
-            DepartureAirport = departureAirport;
+            DepartureAirports = departureAirports;
         }
 
         public List<string> BorderLines { get; }
@@ -37,25 +39,27 @@ namespace Compiler.Model
         public int MaximumAltitude { get; }
         public SectorOwnerHierarchy Owners { get; }
         public List<SectorAlternateOwnerHierarchy> AltOwners { get; }
+        public List<SectorActive> Active { get; }
         public List<SectorGuest> Guests { get; }
         public SectorBorder Border { get; }
-        public List<SectorActiveAirports> ArrivalAirports { get; }
-        public List<SectorActiveAirports> DepartureAirport { get; }
+        public SectorArrivalAirports ArrivalAirports { get; }
+        public SectorDepartureAirports DepartureAirports { get; }
 
         public string Compile()
         {
             return String.Format(
-                "SECTOR:{0}:{1}:{2}{3}\r\n{4}{5}{6}{7}{8}{9}\r\n",
+                "SECTOR:{0}:{1}:{2}{3}\r\n{4}{5}{6}{7}{8}{9}{10}\r\n",
                 this.Name,
                 this.MinimumAltitude.ToString(),
                 this.MaximumAltitude.ToString(),
                 this.CompileComment(),
                 this.Owners.Compile(),
                 this.AltOwners.Aggregate("", (ownerString, newOwner) => ownerString + newOwner.Compile()),
+                this.Active.Aggregate("", (activeString, newActive) => activeString + newActive.Compile()),
                 this.Guests.Aggregate("", (guestString, newGuest) => guestString + newGuest.Compile()),
                 this.Border.Compile(),
-                this.ArrivalAirports.Aggregate("", (airportString, airport) => airportString + airport.Compile()),
-                this.DepartureAirport.Aggregate("", (airportString, airport) => airportString + airport.Compile())
+                this.ArrivalAirports.Compile(),
+                this.DepartureAirports.Compile()
             );
         }
     }
---
+++ tests/CompilerTest/Model/SectorTest.cs
@@ -0,0 +1,149 @@
+﻿using Xunit;
+using Compiler.Model;
+using System.Collections.Generic;
+
+namespace CompilerTest.Model
+{
+    public class SectorTest
+    {
+        private readonly Sector model;
+        private readonly SectorOwnerHierarchy owners;
+        private readonly List<SectorAlternateOwnerHierarchy> altOwners = new List<SectorAlternateOwnerHierarchy>()
+        {
+            new SectorAlternateOwnerHierarchy("ALT1", new List<string>(){"LON1", "LON2"}, ""),
+            new SectorAlternateOwnerHierarchy("ALT2", new List<string>(){"LON3", "LON4"}, ""),
+        };
+        private readonly List<SectorGuest> guests = new List<SectorGuest>()
+        {
+            new SectorGuest("LLN", "EGLL", "EGWU", ""),
+            new SectorGuest("LLS", "EGLL", "*", ""),
+        };
+        private readonly List<SectorActive> active = new List<SectorActive>()
+        {
+            new SectorActive("EGLL", "09R", ""),
+            new SectorActive("EGWU", "05", ""),
+        };
+
+        private readonly SectorBorder border;
+
+        private SectorDepartureAirports departureAirports;
+
+        private SectorArrivalAirports arrivalAirports;
+
+        public SectorTest()
+        {
+            this.owners = new SectorOwnerHierarchy(
+                new List<string>()
+                {
+                    "LLN", "LLS"
+                },
+                ""
+            );
+
+            this.border = new SectorBorder(
+                new List<string>()
+                {
+                    "LINE1",
+                    "LINE2",
+                },
+                ""
+            );
+
+            this.departureAirports = new SectorDepartureAirports(new List<string>() { "EGLL", "EGWU" }, "");
+            this.arrivalAirports = new SectorArrivalAirports(new List<string>() { "EGSS", "EGGW" }, "");
+
+            this.model = new Sector(
+                "COOL",
+                5000,
+                66000,
+                this.owners,
+                this.altOwners,
+                this.active,
+                this.guests,
+                this.border,
+                this.arrivalAirports,
+                this.departureAirports,
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsSectorName()
+        {
+            Assert.Equal("COOL", this.model.Name);
+        }
+
+        [Fact]
+        public void TestItSetsSectorMinimumAltitude()
+        {
+            Assert.Equal(5000, this.model.MinimumAltitude);
+        }
+
+        [Fact]
+        public void TestItSetsSectorMaximumAltitude()
+        {
+            Assert.Equal(66000, this.model.MaximumAltitude);
+        }
+
+        [Fact]
+        public void TestItSetsOwners()
+        {
+            Assert.Equal(this.owners, this.model.Owners);
+        }
+
+        [Fact]
+        public void TestItSetsAltOwners()
+        {
+            Assert.Equal(this.altOwners, this.model.AltOwners);
+        }
+
+        [Fact]
+        public void TestItSetsActive()
+        {
+            Assert.Equal(this.active, this.model.Active);
+        }
+
+        [Fact]
+        public void TestItSetsGuests()
+        {
+            Assert.Equal(this.guests, this.model.Guests);
+        }
+
+        [Fact]
+        public void TestItSetsBorder()
+        {
+            Assert.Equal(this.border, this.model.Border);
+        }
+
+        [Fact]
+        public void TestItSetsArrivalAirports()
+        {
+            Assert.Equal(this.arrivalAirports, this.model.ArrivalAirports);
+        }
+
+        [Fact]
+        public void TestItSetsDepartureAirports()
+        {
+            Assert.Equal(this.departureAirports, this.model.DepartureAirports);
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "SECTOR:COOL:5000:66000 ;comment\r\n" +
+                "OWNER:LLN:LLS\r\n" +
+                "ALTOWNER:ALT1:LON1:LON2\r\n" +
+                "ALTOWNER:ALT2:LON3:LON4\r\n" +
+                "ACTIVE:EGLL:09R\r\n" +
+                "ACTIVE:EGWU:05\r\n" +
+                "GUEST:LLN:EGLL:EGWU\r\n" +
+                "GUEST:LLS:EGLL:*\r\n" +
+                "BORDER:LINE1:LINE2\r\n" +
+                "ARRAPT:EGSS:EGGW\r\n" +
+                "DEPAPT:EGLL:EGWU\r\n\r\n",
+                this.model.Compile()
+            );
+        }
+    }
+}
