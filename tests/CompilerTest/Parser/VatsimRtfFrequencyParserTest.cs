53b8899ea8572615d49df03879ca19304e12fefb
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-27 16:31:41 +0100

    Account For Low RTF Frequencies

--- tests/CompilerTest/Parser/VatsimRtfFrequencyParserTest.cs
+++ tests/CompilerTest/Parser/VatsimRtfFrequencyParserTest.cs
@@ -17,11 +17,12 @@ namespace CompilerTest.Parser
         [InlineData("123.456.789", null)] // Too many dots
         [InlineData("123", null)] // Not enough dots
         [InlineData("abc.500", null)] // First part not an integer
-        [InlineData("116.500", null)] // First part too small
+        [InlineData("106.500", null)] // First part too small
         [InlineData("138.500", null)] // First part too big
         [InlineData("135.abc", null)] // Second part not an integer
         [InlineData("135.155", null)] // Second part doesn't divide
         [InlineData("135.150", "135.150")] // Everything ok
+        [InlineData("108.150", "108.150")] // Everything ok
         [InlineData("199.998", "199.998")] // Everything ok - no frequency
         [InlineData("119.720", "119.720")] // Everything ok - round up
         public void TestItParsesFrequencies(string frequency, string expected)
