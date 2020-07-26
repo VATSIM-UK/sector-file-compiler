6e3b1cd0302ca100fdd65cb15037eda22ae0a5f1
Andy Ford <andyford993@gmail.com>
2020-07-26 20:59:16 +0100

    Add Runway Model

---
+++ src/Compiler/Model/Runway.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Compiler.Model
+{
+    public class Runway : AbstractSectorElement, ICompilable
+    {
+
+        public Runway(
+            string firstIdentifier,
+            int firstHeading,
+            Coordinate firstThreshold,
+            string reverseIdentifier,
+            int reverseHeading,
+            Coordinate reverseThreshold,
+            string runwayDialogDescription,
+            string comment
+        ) 
+            : base(comment) 
+        {
+            FirstIdentifier = firstIdentifier;
+            FirstHeading = firstHeading;
+            FirstThreshold = firstThreshold;
+            ReverseIdentifier = reverseIdentifier;
+            ReverseHeading = reverseHeading;
+            ReverseThreshold = reverseThreshold;
+            RunwayDialogDescription = runwayDialogDescription;
+        }
+
+        public string FirstIdentifier { get; }
+        public int FirstHeading { get; }
+        public Coordinate FirstThreshold { get; }
+        public string ReverseIdentifier { get; }
+        public int ReverseHeading { get; }
+        public Coordinate ReverseThreshold { get; }
+        public string RunwayDialogDescription { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "{0} {1} {2} {3} {4} {5} {6}{7}\r\n",
+                this.FirstIdentifier,
+                this.ReverseIdentifier,
+                this.FormatHeading(this.FirstHeading),
+                this.FormatHeading(this.ReverseHeading),
+                this.FirstThreshold.ToString(),
+                this.ReverseThreshold.ToString(),
+                this.RunwayDialogDescription,
+                this.CompileComment()
+            );
+        }
+
+        private string FormatHeading(int heading)
+        {
+            return heading.ToString().PadLeft(3, '0');
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/RunwayTest.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using Compiler.Model;
+
+namespace CompilerTest.Model
+{
+    public class RunwayTest
+    {
+        private readonly Runway runway;
+
+        public RunwayTest()
+        {
+            this.runway = new Runway(
+                "09",
+                90,
+                new Coordinate("abc", "def"),
+                "27",
+                270,
+                new Coordinate("ghi", "jkl"),
+                "EGGD - Bristol",
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsFirstIdentifier()
+        {
+            Assert.Equal("09", this.runway.FirstIdentifier);
+        }
+
+        [Fact]
+        public void TestItSetsFirstHeading()
+        {
+            Assert.Equal(90, this.runway.FirstHeading);
+        }
+
+        [Fact]
+        public void TestItSetsFirstThreshold()
+        {
+            Assert.Equal(new Coordinate("abc", "def"), this.runway.FirstThreshold);
+        }
+
+        [Fact]
+        public void TestItSetsReverseIdentifier()
+        {
+            Assert.Equal("27", this.runway.ReverseIdentifier);
+        }
+
+        [Fact]
+        public void TestItSetsReserveHeading()
+        {
+            Assert.Equal(270, this.runway.ReverseHeading);
+        }
+
+        [Fact]
+        public void TestItSetsReverseThreshold()
+        {
+            Assert.Equal(new Coordinate("ghi", "jkl"), this.runway.ReverseThreshold);
+        }
+
+        [Fact]
+        public void TestItSetsRunwayDialogDescription()
+        {
+            Assert.Equal("EGGD - Bristol", this.runway.RunwayDialogDescription);
+        }
+
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "09 27 090 270 abc def ghi jkl EGGD - Bristol ;comment\r\n",
+                this.runway.Compile()
+            );
+        }
+    }
+}
