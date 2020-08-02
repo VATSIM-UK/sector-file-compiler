6d2ccc4b297876c577e882887ee21856c07458cc
Andy Ford <andyford993@gmail.com>
2020-08-02 18:35:36 +0100

    Add GeoSegment

---
+++ src/Compiler/Model/GeoSegment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class GeoSegment : AbstractSectorElement, ICompilable
+    {
+        public GeoSegment(Coordinate firstCoordinate, Coordinate secondCoordinate, string colour, string comment)
+            : base(comment)
+        {
+            FirstCoordinate = firstCoordinate;
+            SecondCoordinate = secondCoordinate;
+            Colour = colour;
+        }
+
+        public Coordinate FirstCoordinate { get; }
+        public Coordinate SecondCoordinate { get; }
+        public string Colour { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "{0} {1} {2}{3}\r\n",
+                this.FirstCoordinate.ToString(),
+                this.SecondCoordinate.ToString(),
+                this.Colour,
+                this.CompileComment()
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/GeoSegmentTest.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using Compiler.Model;
+
+namespace CompilerTest.Model
+{
+    public class GeoSegmentTest
+    {
+        private GeoSegment segment;
+
+        public GeoSegmentTest()
+        {
+            this.segment = new GeoSegment(
+                new Coordinate("abc", "def"),
+                new Coordinate("ghi", "jkl"),
+                "red",
+                "comment"
+            );
+        }
+
+        [Fact]
+        public void TestItSetsFirstCoordinate()
+        {
+            Assert.Equal(new Coordinate("abc", "def"), this.segment.FirstCoordinate);
+        }
+
+        [Fact]
+        public void TestItSetsSecondCoordinate()
+        {
+            Assert.Equal(new Coordinate("ghi", "jkl"), this.segment.SecondCoordinate);
+        }
+
+        [Fact]
+        public void TestItSetsColour()
+        {
+            Assert.Equal("red", this.segment.Colour);
+        }
+
+        [Fact]
+        public void TestItCompilesWithComment()
+        {
+            Assert.Equal("abc def ghi jkl red ;comment\r\n", this.segment.Compile());
+        }
+
+        [Fact]
+        public void TestItCompilesWithNoComment()
+        {
+            GeoSegment segment = new GeoSegment(
+                new Coordinate("abc", "def"),
+                new Coordinate("ghi", "jkl"),
+                "red",
+                ""
+            );
+            Assert.Equal("abc def ghi jkl red\r\n", segment.Compile());
+        }
+    }
+}
