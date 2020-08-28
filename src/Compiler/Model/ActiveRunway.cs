2395b1a568c819adf784294f879a01893266aeb2
Andy Ford <andyford993@gmail.com>
2020-08-28 21:27:49 +0100

    Add Model For Active Runway

---
+++ src/Compiler/Model/ActiveRunway.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Compiler.Model
+{
+    public class ActiveRunway : AbstractSectorElement, ICompilable
+    {
+        public ActiveRunway(string identifier, string airfield, int mode) : base("")
+        {
+            Identifier = identifier;
+            Airfield = airfield;
+            Mode = mode;
+        }
+
+        public string Identifier { get; }
+        public string Airfield { get; }
+        public int Mode { get; }
+        public bool ForDeparture { get; }
+
+        public string Compile()
+        {
+            return String.Format(
+                "ACTIVE_RUNWAY:{0}:{1}:{2}\r\n",
+                this.Airfield,
+                this.Identifier,
+                this.Mode
+            );
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/ActiveRunwayTest.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using Compiler.Model;
+
+namespace CompilerTest.Model
+{
+    public class ActiveRunwayTest
+    {
+        private readonly ActiveRunway activeRunway;
+
+        public ActiveRunwayTest()
+        {
+            this.activeRunway = new ActiveRunway(
+                "33",
+                "EGBB",
+                1
+            );
+        }
+
+        [Fact]
+        public void TestItSetsIdentifier()
+        {
+            Assert.Equal("33", this.activeRunway.Identifier);
+        }
+
+        [Fact]
+        public void TestItSetsAirfield()
+        {
+            Assert.Equal("EGBB", this.activeRunway.Airfield);
+        }
+
+        [Fact]
+        public void TestItSetsMode()
+        {
+            Assert.Equal(1, this.activeRunway.Mode);
+        }
+
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "ACTIVE_RUNWAY:EGBB:33:1\r\n",
+                this.activeRunway.Compile()
+            );
+        }
+    }
+}
