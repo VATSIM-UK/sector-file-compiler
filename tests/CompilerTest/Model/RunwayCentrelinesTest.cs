5355428ece06da536f0063feca80d71038967fbb
Andy Ford <andyford993@gmail.com>
2020-09-17 15:29:32 +0100

    Add Runway Centrelines

---
+++ tests/CompilerTest/Model/RunwayCentrelinesTest.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using Compiler.Model;
+using System.Collections.Generic;
+
+namespace CompilerTest.Model
+{
+    public class RunwayCentrelinesTest
+    {
+        private readonly RunwayCentrelines centrelines;
+
+        public RunwayCentrelinesTest()
+        {
+            this.centrelines = new RunwayCentrelines(
+                new List<Coordinate>
+                {
+                    new Coordinate("abc", "def"),
+                    new Coordinate("ghi", "jkl"),
+                },
+                ""
+            );
+        }
+
+        [Fact]
+        public void TestItSetsCoordinates()
+        {
+            Assert.Equal(
+                new List<Coordinate>
+                {
+                    new Coordinate("abc", "def"),
+                    new Coordinate("ghi", "jkl"),
+                },
+                this.centrelines.Coordinates
+            );
+        }
+
+        [Fact]
+        public void TestItCompiles()
+        {
+            Assert.Equal(
+                "abc def\r\nghi jkl\r\n",
+                this.centrelines.Compile()
+            );
+        }
+    }
+}
