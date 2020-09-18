394ec4047cedc23164bb45b6d67002dffcfe1367
Andy Ford <andyford993@gmail.com>
2020-09-18 12:44:10 +0100

    Add Docblock Model

---
+++ src/Compiler/Model/Docblock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Model
+{
+    public class Docblock : IEnumerable<Comment>
+    {
+        protected List<Comment> Lines { get; set; }
+
+        public Docblock()
+        {
+            this.Lines = new List<Comment>();
+        }
+
+        public void AddLine(Comment line)
+        {
+            this.Lines.Add(line);
+        }
+
+        public IEnumerator<Comment> GetEnumerator()
+        {
+            return ((IEnumerable<Comment>)Lines).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/DocblockTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using Compiler.Model;
+using System.Collections.Generic;
+
+namespace CompilerTest.Model
+{
+    public class DocblockTest
+    {
+        [Fact]
+        public void TestItAddsLines()
+        {
+            Comment line1 = new Comment("Line 1");
+            Comment line2 = new Comment("Line 2");
+            Comment line3 = new Comment("Line 3");
+            Comment line4 = new Comment("Line 4");
+            Docblock docblock = new Docblock();
+            docblock.AddLine(line1);
+            docblock.AddLine(line2);
+            docblock.AddLine(line3);
+            docblock.AddLine(line4);
+
+            IEnumerator<Comment> enumerator = docblock.GetEnumerator();
+            enumerator.MoveNext();
+            Assert.Equal(line1, enumerator.Current);
+            enumerator.MoveNext();
+            Assert.Equal(line2, enumerator.Current);
+            enumerator.MoveNext();
+            Assert.Equal(line3, enumerator.Current);
+            enumerator.MoveNext();
+            Assert.Equal(line4, enumerator.Current);
+        }
+    }
+}
