c3870a522cf842c4051fd02178d99fcc72e58b3b
Andy Ford <andyford993@gmail.com>
2020-09-27 15:45:24 +0100

    Add Exception For Invalid Config File

---
+++ src/Compiler/Exception/ConfigFileInvalidException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler.Exception
+{
+    class ConfigFileInvalidException: ArgumentException
+    {
+        public ConfigFileInvalidException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
