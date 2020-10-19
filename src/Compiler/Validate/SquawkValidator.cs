f9cc2c7e826c3a3cc67f9c7a48fb643f57632753
Andy Ford <andyford993@gmail.com>
2020-10-19 19:19:41 +0100

    Add Dedicated Validator For Squawk Codes

---
+++ src/Compiler/Validate/SquawkValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Compiler.Validate
+{
+    /*
+     * Validates that squawk codes are valid
+     */
+    public class SquawkValidator
+    {
+        private static readonly Regex squawkRegex = new Regex(@"^[0-7]{4}$");
+
+        public static bool SquawkValid(string squawk)
+        {
+            return squawkRegex.IsMatch(squawk);
+        }
+    }
+}
---
+++ tests/CompilerTest/Validate/SquawkValidatorTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using Compiler.Validate;
+
+namespace CompilerTest.Validate
+{
+    public class SquawkValidatorTest
+    {
+        [Theory]
+        [InlineData("0")]
+        [InlineData("00")]
+        [InlineData("000")]
+        [InlineData("00000")]
+        [InlineData("1358")]
+        [InlineData("8351")]
+        [InlineData("1851")]
+        [InlineData("1381")]
+        [InlineData("abcd")]
+        public void TestValidationFailure(string input)
+        {
+            Assert.False(SquawkValidator.SquawkValid(input));
+        }
+
+        [Theory]
+        [InlineData("0000")]
+        [InlineData("7777")]
+        [InlineData("1234")]
+        [InlineData("3241")]
+        [InlineData("7657")]
+        [InlineData("7653")]
+        [InlineData("0017")]
+        public void TestValidationSuccess(string input)
+        {
+            Assert.True(SquawkValidator.SquawkValid(input));
+        }
+    }
+}
