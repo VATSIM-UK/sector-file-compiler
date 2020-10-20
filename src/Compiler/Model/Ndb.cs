ef185354faeb0397011a2aa175a3c933f2be1bdd
Andy Ford <andyford993@gmail.com>
2020-10-20 19:12:50 +0100

    Convert Ndb To New Format

--- src/Compiler/Model/Ndb.cs
+++ src/Compiler/Model/Ndb.cs
@@ -4,27 +4,33 @@ using System.Text;
 
 namespace Compiler.Model
 {
-    public class Ndb : AbstractCompilableElement, ICompilable
+    public class Ndb : AbstractCompilableElement
     {
         public string Identifier { get; }
         public string Frequency { get; }
         public Coordinate Coordinate { get; }
 
-        public Ndb(string identifier, string frequency, Coordinate coordinate, string comment) : base(comment)
+        public Ndb(
+            string identifier,
+            string frequency,
+            Coordinate coordinate,
+            Definition definition,
+            Docblock docblock,
+            Comment inlineComment
+        ) : base (definition, docblock, inlineComment)
         {
             this.Identifier = identifier;
             this.Frequency = frequency;
             this.Coordinate = coordinate;
         }
 
-        public string Compile()
+        public override string GetCompileData()
         {
             return string.Format(
-                "{0} {1} {2}{3}\r\n",
+                "{0} {1} {2}",
                 this.Identifier,
                 this.Frequency,
-                this.Coordinate.ToString(),
-                this.CompileComment()
+                this.Coordinate.ToString()
             );
         }
     }
