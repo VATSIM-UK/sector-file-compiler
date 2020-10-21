d61a751ecc95ef5908e1cc90aa2ca5ea4a65fb7f
Andy Ford <andyford993@gmail.com>
2020-10-21 17:58:05 +0100

    Convert VOR

--- src/Compiler/Model/Vor.cs
+++ src/Compiler/Model/Vor.cs
@@ -4,27 +4,33 @@ using System.Text;
 
 namespace Compiler.Model
 {
-    public class Vor : AbstractCompilableElement, ICompilable
+    public class Vor : AbstractCompilableElement
     {
         public string Identifier { get; }
         public string Frequency { get; }
         public Coordinate Coordinate { get; }
 
-        public Vor(string identifier, string frequency, Coordinate coordinate, string comment) : base(comment)
+        public Vor(
+            string identifier,
+            string frequency,
+            Coordinate coordinate,
+            Definition definition,
+            Docblock docblock,
+            Comment inlineComment
+        ) : base(definition, docblock, inlineComment)
         {
             this.Identifier = identifier;
             this.Frequency = frequency;
             this.Coordinate = coordinate;
         }
 
-        public string Compile()
+        public override string GetCompileData()
         {
             return string.Format(
-                "{0} {1} {2}{3}\r\n",
+                "{0} {1} {2}",
                 this.Identifier,
                 this.Frequency,
-                this.Coordinate.ToString(),
-                this.CompileComment()
+                this.Coordinate.ToString()
             );
         }
     }
--- src/Compiler/Parser/VorParser.cs
+++ src/Compiler/Parser/VorParser.cs
@@ -11,75 +11,70 @@ namespace Compiler.Parser
     public class VorParser : AbstractSectorElementParser, ISectorDataParser
     {
         private readonly SectorElementCollection elements;
-        private readonly ISectorLineParser sectorLineParser;
         private readonly IFrequencyParser frequencyParser;
         private readonly IEventLogger eventLogger;
 
         public VorParser(
-            MetadataParser metadataParser,
-            ISectorLineParser sectorLineParser,
             IFrequencyParser frequencyParser,
             SectorElementCollection elements,
             IEventLogger eventLogger
-        ) : base(metadataParser)
-        {
+        ) {
             this.elements = elements;
-            this.sectorLineParser = sectorLineParser;
             this.frequencyParser = frequencyParser;
             this.eventLogger = eventLogger;
         }
 
         public void ParseData(AbstractSectorDataFile data)
         {
-            foreach (string line in data)
+            foreach (SectorData line in data)
             {
-                // Defer all metadata lines to the base
-                if (this.ParseMetadata(line))
-                {
-                    continue;
-                }
-
-                SectorFormatLine sectorData = this.sectorLineParser.ParseLine(line);
-                if (sectorData.dataSegments.Count != 4)
+                if (line.dataSegments.Count != 4)
                 {
                     this.eventLogger.AddEvent(
-                        new SyntaxError("Incorrect number of VOR segments", data.FullPath, data.CurrentLineNumber)
+                        new SyntaxError("Incorrect number of VOR segments", line)
                     );
                     continue;
                 }
 
                 // Check the identifier
                 if (
-                    sectorData.dataSegments[0].Any(char.IsDigit) ||
-                    (sectorData.dataSegments[0].Length != 2 && sectorData.dataSegments[0].Length != 3)
+                    line.dataSegments[0].Any(char.IsDigit) ||
+                    (line.dataSegments[0].Length != 2 && line.dataSegments[0].Length != 3)
                 ) {
                     this.eventLogger.AddEvent(
-                        new SyntaxError("Invalid VOR identifier: " + sectorData.dataSegments[1], data.FullPath, data.CurrentLineNumber)
+                        new SyntaxError("Invalid VOR identifier: " + line.dataSegments[1], line)
                     );
                     return;
                 }
 
                 // Parse the frequency
-                if (this.frequencyParser.ParseFrequency(sectorData.dataSegments[1]) == null)
+                if (this.frequencyParser.ParseFrequency(line.dataSegments[1]) == null)
                 {
                     this.eventLogger.AddEvent(
-                        new SyntaxError("Invalid VOR frequency: " + sectorData.dataSegments[1], data.FullPath, data.CurrentLineNumber)
+                        new SyntaxError("Invalid VOR frequency: " + line.dataSegments[1], line)
                     );
                     return;
                 }
 
                 // Parse the coordinate
-                Coordinate parsedCoordinate = CoordinateParser.Parse(sectorData.dataSegments[2], sectorData.dataSegments[3]);
+                Coordinate parsedCoordinate = CoordinateParser.Parse(line.dataSegments[2], line.dataSegments[3]);
                 if (parsedCoordinate.Equals(CoordinateParser.invalidCoordinate))
                 {
                     this.eventLogger.AddEvent(
-                        new SyntaxError("Invalid coordinate format: " + line, data.FullPath, data.CurrentLineNumber)
+                        new SyntaxError("Invalid coordinate format: " + data.CurrentLine, line)
                     );
                     return;
                 }
 
                 this.elements.Add(
-                    new Vor(sectorData.dataSegments[0], sectorData.dataSegments[1], parsedCoordinate, sectorData.comment)
+                    new Vor(
+                        line.dataSegments[0],
+                        line.dataSegments[1],
+                        parsedCoordinate,
+                        line.definition,
+                        line.docblock,
+                        line.inlineComment
+                    )
                 );
             }
         }
