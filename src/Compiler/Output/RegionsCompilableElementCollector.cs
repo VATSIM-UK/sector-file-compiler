9405dd556a7cd65e1f41b8e759bc836640ce83c1
Andy Ford <andyford993@gmail.com>
2020-10-30 18:12:27 +0000

    Implement Regions Collector

---
+++ src/Compiler/Output/RegionsCompilableElementCollector.cs
@@ -0,0 +1,28 @@
+﻿using Compiler.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Compiler.Output
+{
+    public class RegionsCompilableElementCollector : ICompilableElementCollector
+    {
+        private readonly SectorElementCollection sectorElements;
+        private readonly OutputGroupRepository repository;
+
+        public RegionsCompilableElementCollector(SectorElementCollection sectorElements, OutputGroupRepository repository)
+        {
+            this.sectorElements = sectorElements;
+            this.repository = repository;
+        }
+
+        public IEnumerable<IGrouping<OutputGroup, ICompilableElementProvider>> GetCompilableElements(OutputSections section)
+        {
+            return this.sectorElements.Regions.GroupBy(
+                region => this.repository.GetForDefinitionFile(region.GetDefinition()),
+                region => region
+            );
+        }
+    }
+}
