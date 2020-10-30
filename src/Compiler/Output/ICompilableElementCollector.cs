d65313754b0c4ad737b2e564828581f8f4eabc9d
Andy Ford <andyford993@gmail.com>
2020-10-30 18:11:32 +0000

    Add Interface For Compilable Element Collector

---
+++ src/Compiler/Output/ICompilableElementCollector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compiler.Model;
+
+namespace Compiler.Output
+{
+    /*
+     * An interface that collects compilable elements according to their output group
+     * so that they can then be outputted.
+     */
+    public interface ICompilableElementCollector
+    {
+        public IEnumerable<IGrouping<OutputGroup, ICompilableElementProvider>> GetCompilableElements(
+            OutputSections section
+        );
+    }
+}
