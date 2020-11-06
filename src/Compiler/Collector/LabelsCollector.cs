a7bd359bf69d20fb4f42a7eec2b25365b021525d
Andy Ford <andyford993@gmail.com>
2020-11-06 20:32:19 +0000

    Add Labels Collector

---
+++ src/Compiler/Collector/LabelsCollector.cs
@@ -0,0 +1,28 @@
+﻿using Compiler.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Compiler.Output
+{
+    public class LabelsCollector : ICompilableElementCollector
+    {
+        private readonly SectorElementCollection sectorElements;
+        private readonly OutputGroupRepository repository;
+
+        public LabelsCollector(SectorElementCollection sectorElements, OutputGroupRepository repository)
+        {
+            this.sectorElements = sectorElements;
+            this.repository = repository;
+        }
+
+        public IEnumerable<IGrouping<OutputGroup, ICompilableElementProvider>> GetCompilableElements()
+        {
+            return this.sectorElements.Labels.GroupBy(
+                label => this.repository.GetForDefinitionFile(label.GetDefinition()),
+                label => label
+            );
+        }
+    }
+}
