26e22925f658022d290f68cad9b2bb16267b6932
Andy Ford <andyford993@gmail.com>
2020-12-12 19:41:55 +0000

    Sections

--- src/Compiler/Config/MiscConfigFileSections.cs
+++ src/Compiler/Config/MiscConfigFileSections.cs
@@ -8,14 +8,14 @@ namespace Compiler.Config
         public static readonly List<ConfigFileSection> configFileSections = new List<ConfigFileSection>
         {
             new ConfigFileSection("agreements", InputDataType.ESE_AGREEMENTS, "Agreements"),
-            new ConfigFileSection("freetext", InputDataType.ESE_FREETEXT, "Misc Freetext"),
+            new ConfigFileSection("freetext", InputDataType.ESE_FREETEXT, "Freetext"),
             new ConfigFileSection("colours", InputDataType.SCT_COLOUR_DEFINITIONS, "Colour Definitions"),
             new ConfigFileSection("info", InputDataType.SCT_INFO),
             new ConfigFileSection("file_headers", InputDataType.FILE_HEADERS),
             new ConfigFileSection("pre_positions", InputDataType.ESE_PRE_POSITIONS),
-            new ConfigFileSection("fixes", InputDataType.SCT_FIXES, "Fixes"),
-            new ConfigFileSection("ndbs", InputDataType.SCT_NDBS, "NDBs"),
-            new ConfigFileSection("vors", InputDataType.SCT_VORS, "VORs"),
+            new ConfigFileSection("fixes", InputDataType.SCT_FIXES, null),
+            new ConfigFileSection("ndbs", InputDataType.SCT_NDBS, null),
+            new ConfigFileSection("vors", InputDataType.SCT_VORS, null),
             new ConfigFileSection("danger_areas", InputDataType.SCT_ARTCC, "Danger Areas"),
             new ConfigFileSection("artcc_low", InputDataType.SCT_ARTCC_LOW, "Low ARTCCs"),
             new ConfigFileSection("artcc_high", InputDataType.SCT_ARTCC_HIGH, "High ARTCCs"),
