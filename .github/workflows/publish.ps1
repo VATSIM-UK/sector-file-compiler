c23e2a9445c5dba22415da41bd732d73c882ed0e
Andy Ford <andyford993@gmail.com>
2021-01-26 19:16:07 +0000

    build: sort out versions

---
+++ .github/workflows/publish.ps1
@@ -0,0 +1,14 @@
+if (${env:GITHUB_REF} != "refs/head/main")
+{
+    echo ${env:GITHUB_REF}
+    $buildVersion = "1.0.0-${env:GITHUB_SHA}"
+} else {
+    $semanticRelease = npx semantic-release@17 --dry-run | Select-String -Pattern "Published release ((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)"
+    $buildVersion = $semanticRelease.Matches.Groups[1].value
+}
+
+echo "Building version $buildVersion"
+
+dotnet publish -r win-x64 -c Release -p:Version="$buildVersion" -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:SelfContained=true -p:PublishTrimmed=true -o publish/windows src/CompilerCli/CompilerCli.csproj
+dotnet publish -r linux-x64 -c Release -p:Version="$buildVersion" -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:SelfContained=true -p:PublishTrimmed=true -o publish/linux src/CompilerCli/CompilerCli.csproj
+dotnet publish -r osx-x64 -c Release -p:Version="$buildVersion" -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:SelfContained=true -p:PublishTrimmed=true -o publish/osx src/CompilerCli/CompilerCli.csproj
