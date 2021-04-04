96e4b816eadc549ebdb3eded1a195030ab8f7f23
Andy Ford <andyford993@gmail.com>
2021-04-04 17:35:01 +0100

    feat(radar): add models to record RADAR2 data

---
+++ src/Compiler/Model/RadarParameters.cs
@@ -0,0 +1,37 @@
+﻿namespace Compiler.Model
+{
+    public class RadarParameters
+    {
+        public int? Range { get; }
+        public int? Altitude { get; }
+        public int? ConeSlope { get; }
+
+        public RadarParameters(
+            int range,
+            int altitude,
+            int coneSlope
+        ) {
+            Range = range;
+            Altitude = altitude;
+            ConeSlope = coneSlope;
+        }
+
+        public RadarParameters()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{Range}:{Altitude}:{ConeSlope}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RadarParameters &&
+                   (obj as RadarParameters).Altitude == Altitude &&
+                   (obj as RadarParameters).Range == Range &&
+                   (obj as RadarParameters).ConeSlope == ConeSlope;
+        }
+    }
+}
---
+++ tests/CompilerTest/Model/RadarParametersTest.cs
@@ -0,0 +1,43 @@
+﻿using Compiler.Model;
+using Xunit;
+
+namespace CompilerTest.Model
+{
+    public class RadarParametersTest
+    {
+        [Fact]
+        public void ItHasRange()
+        {
+            RadarParameters parameters = new(1, 2, 3);
+            Assert.Equal(1, parameters.Range);
+        }
+        
+        [Fact]
+        public void ItHasAltitude()
+        {
+            RadarParameters parameters = new(1, 2, 3);
+            Assert.Equal(2, parameters.Altitude);
+        }
+        
+        [Fact]
+        public void ItHasConeSlope()
+        {
+            RadarParameters parameters = new(1, 2, 3);
+            Assert.Equal(3, parameters.ConeSlope);
+        }
+        
+        [Fact]
+        public void ItHasFormatsAsStringWithValues()
+        {
+            RadarParameters parameters = new(1, 2, 3);
+            Assert.Equal("1:2:3", parameters.ToString());
+        }
+        
+        [Fact]
+        public void ItHasFormatsAsStringWithNoValues()
+        {
+            RadarParameters parameters = new();
+            Assert.Equal("::", parameters.ToString());
+        }
+    }
+}
