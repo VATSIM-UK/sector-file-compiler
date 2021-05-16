59ba355eb2b5c7aa3a01266cb9915b2ce4a9aaf3
Andy Ford <andyford993@gmail.com>
2021-05-16 16:13:57 +0100

    feat(sids): validate the runways used on sids and stars
    Used to use just airfield, now do both.

--- src/Compiler/Validate/AllSidsMustHaveAValidRunway.cs
+++ src/Compiler/Validate/AllSidsMustHaveAValidRunway.cs
@@ -1,25 +1,19 @@
-﻿using System.Linq;
-using Compiler.Event;
+﻿using Compiler.Event;
 using Compiler.Error;
 using Compiler.Model;
 using Compiler.Argument;
 
 namespace Compiler.Validate
 {
-    public class AllSidsMustHaveAValidAirport : IValidationRule
+    public class AllSidsMustHaveAValidRunway : AbstractCoordinationPointRunwayChecker, IValidationRule
     {
         public void Validate(SectorElementCollection sectorElements, CompilerArguments args, IEventLogger events)
         {
             foreach(SidStar sidStar in sectorElements.SidStars)
             {
-                bool airportExists = sectorElements.Airports
-                    .Count(airport => sidStar.Airport == airport.Icao) != 0;
-
-                if (!airportExists)
-                {
+                if (!RunwayValid(sectorElements, sidStar.Runway, sidStar.Airport)) {
                     string errorMessage =
-                        $"Airport {sidStar.Airport} is not valid for {sidStar.Type}/{sidStar.Identifier}";
-
+                        $"Runway {sidStar.Airport}/{sidStar.Runway} is not valid for {sidStar.Type}/{sidStar.Identifier}";
                     events.AddEvent(new ValidationRuleFailure(errorMessage, sidStar));
                 }
             }
---
+++ tests/CompilerTest/Validate/AllSidsMustHaveAValidRunwayTest.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using Compiler.Model;
+using Compiler.Validate;
+using CompilerTest.Bogus.Factory;
+
+namespace CompilerTest.Validate
+{
+    public class AllSidsMustHaveAValidRunwayTest: AbstractValidatorTestCase
+    {
+        private readonly SidStar sid1;
+        private readonly SidStar sid2;
+        private readonly SidStar sid3;
+        private readonly SidStar sid4;
+
+        public AllSidsMustHaveAValidRunwayTest()
+        {
+            sid1 = SidStarFactory.Make(airport: "EGKK", runway: "26L");
+            sid2 = SidStarFactory.Make(airport: "EGCC", runway: "23R");
+            sid3 = SidStarFactory.Make(airport: "EGBB", runway: "16");
+            sid4 = SidStarFactory.Make(airport: "EGKB", runway: "08");
+
+            sectorElements.Add(AirportFactory.Make("EGKK"));
+            sectorElements.Add(AirportFactory.Make("EGCC"));
+            sectorElements.Add(AirportFactory.Make("EGBB"));
+            sectorElements.Add(RunwayFactory.Make("EGKK", "26L", "08R"));
+            sectorElements.Add(RunwayFactory.Make("EGCC", "23R", "05L"));
+            sectorElements.Add(RunwayFactory.Make("EGBB", "33", "15"));
+        }
+
+        [Fact]
+        public void TestItPassesOnAllValid()
+        {
+            sectorElements.Add(sid1);
+            sectorElements.Add(sid2);
+
+            AssertNoValidationErrors();
+        }
+
+        [Fact]
+        public void TestItFailsOnInvalidAirport()
+        {
+            sectorElements.Add(sid1);
+            sectorElements.Add(sid2);
+            sectorElements.Add(sid3);  // Airport exists, not runway
+            sectorElements.Add(sid4);  // Airport doesn't exist
+
+            AssertValidationErrors(2);
+        }
+
+        protected override IValidationRule GetValidationRule()
+        {
+            return new AllSidsMustHaveAValidRunway();
+        }
+    }
+}
