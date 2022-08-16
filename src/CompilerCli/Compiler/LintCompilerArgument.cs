f2c4d0ea1945a5e052f1b74b9c9d8f57d763d476
Andy Ford <andyford993@gmail.com>
2022-08-16 21:21:22 +0100

    Add tests for running mode options

---
+++ src/CompilerCli/Compiler/LintCompilerArgument.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Compiler.Argument;
+
+namespace CompilerCli.Compiler
+{
+    public class LintCompilerArgument: AbstractCompilerArgument
+    {
+        public override void Parse(List<string> values, CompilerArguments compilerSettings)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetSpecifier()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ tests/CompilerCliTest/Compiler/ValidateCompilerArgumentTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Compiler.Argument;
+using CompilerCli.Compiler;
+using Xunit;
+
+namespace CompilerCliTest.Compiler
+{
+    public class ValidateCompilerArgumentTest
+    {
+        private CompilerArguments arguments;
+        private ValidateCompilerArgument validateCompilerArgument;
+
+        public ValidateCompilerArgumentTest()
+        {
+            arguments = new CompilerArguments();
+            validateCompilerArgument = new ValidateCompilerArgument();
+        }
+
+        [Fact]
+        public void TestItSetsCompileOnlyAsMode()
+        {
+            validateCompilerArgument.Parse(new List<string>(), arguments);
+            Assert.Equal(RunMode.VALIDATE, arguments.Mode);
+        }
+        
+        [Fact]
+        public void TestItThrowsExceptionOnValues()
+        {
+            Assert.Throws<ArgumentException>(
+                () => validateCompilerArgument.Parse(new List<string>(new[] { "a"}), arguments)
+            );
+        }
+        
+        [Fact]
+        public void TestItReturnsASpecifier()
+        {
+            Assert.Equal("--validate", validateCompilerArgument.GetSpecifier());
+        }
+    }
+}
