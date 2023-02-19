331486469a2eb3f3522d9f933496e07f69382a13
Andy Ford <andyford993@gmail.com>
2023-02-19 13:11:54 +0000

    feat: pretty printing
    Adds the ability to insert an extra newline between large elements such as SECTOR
    when printing, which provides improved readability.
    
    Fixes #163

---
+++ tests/CompilerCliTest/Compiler/PrettyCompilerArgumentTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Compiler.Argument;
+using CompilerCli.Compiler;
+using Xunit;
+
+namespace CompilerCliTest.Compiler;
+
+public class PrettyCompilerArgumentTest
+{
+    private CompilerArguments arguments;
+    private PrettyCompilerArgument prettyCompilerArgument;
+
+    public PrettyCompilerArgumentTest()
+    {
+        arguments = new CompilerArguments();
+        prettyCompilerArgument = new PrettyCompilerArgument();
+    }
+
+    [Fact]
+    public void TestItSetsPrettyAsMode()
+    {
+        prettyCompilerArgument.Parse(new List<string>(), arguments);
+        Assert.Equal(Pretty.PRETTY, arguments.Pretty);
+    }
+
+    [Fact]
+    public void TestItThrowsExceptionOnValues()
+    {
+        Assert.Throws<ArgumentException>(
+            () => prettyCompilerArgument.Parse(new List<string>(new[] { "a"}), arguments)
+        );
+    }
+        
+    [Fact]
+    public void TestItReturnsASpecifier()
+    {
+        Assert.Equal("--pretty", prettyCompilerArgument.GetSpecifier());
+    }
+}
