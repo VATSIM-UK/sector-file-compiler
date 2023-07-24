3630265a21e6b4fb977be31a144aaa6abd74b65c
Alice <oliverlford@icloud.com>
2023-07-24 16:38:29 +0100

    HelpArgumentTest

---
+++ tests/CompilerCliTest/Cli/HelpArgumentTest.cs
@@ -0,0 +1,41 @@
+﻿using CompilerCli.Cli;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CompilerCliTest.Cli
+{
+    public class HelpArgumentTest
+    {
+        private CliArguments arguments;
+        private HelpArgument commandLineArgument;
+
+        public HelpArgumentTest() {
+            arguments = new CliArguments();
+            commandLineArgument = new HelpArgument();
+        }
+
+        [Fact]
+        public void TestItSetsHelpExitFlag() {
+            commandLineArgument.Parse(new List<string>(), arguments);
+            Assert.True(arguments.PauseOnFinish);
+        }
+
+        [Fact]
+        public void TestItThrowsExceptionOnTooManyValues() {
+            Assert.Throws<ArgumentException>(
+                () => commandLineArgument.Parse(new List<string>(new[] { "a", "b" }), arguments)
+            );
+        }
+
+        [Fact]
+        public void TestItReturnsASpecifier() {
+            Assert.Equal("--help", commandLineArgument.GetSpecifier());
+        }
+
+        [Fact]
+        public void TestItReturnsAHelpMessage() {
+            Assert.IsType<string>(HelpArgument.GetHelpMessage());
+        }
+    }
+}
